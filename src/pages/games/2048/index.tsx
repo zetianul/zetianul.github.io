f0b29f286b0f828d5776bc3df09096c3fb88db20
zetianul <ul_sniper@foxmail.com>
2020-12-10 19:50:22 +0800

    2048

--- src/pages/games/2048/index.tsx
+++ src/pages/games/2048/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import './style.less';
 
+interface Item{
+  number: number;
+}
+
 export default () => {
 
-  const [lines, setLines] = useState<{number: Number}[][]>(() => {
+  const [lines, setLines] = useState<Item[][]>(() => {
     const temp = [];
     for(let i = 0; i < 4; i++){
       temp.push([])
       for(let j = 0; j < 4; j++){
-        temp[i].push({number: 0})
+        temp[i].push({number: 1})
       }
     }
     return temp;
@@ -18,19 +22,37 @@ export default () => {
 
   } 
 
-  const handleMove = () => {
-    const newLines = JSON.parse(JSON.stringify(lines)); // deepcopy
+  const handleMove = (transform: (x: number, y: number) => [a: number, b: number] = (x, y) => [x, y] ) => {
+    const newLines:Item[][] = [] // deepcopy
+
+    const animation: [x1:number, y1: number, x2: number, y2:number][] = [];
 
     for(let i = 0; i< 4; i++){
+      newLines.push([])
       for(let j = 0; j< 4; j++){
-        
+        const [x1, y1] = transform(i, j)
+        if(lines[x1][y1].number !== 0){
+          if(newLines[i].length > 0 && newLines[i][newLines[i].length - 1].number === lines[x1][y1].number){
+            newLines[i][newLines[i].length - 1].number *= 2
+            animation.push([x1, y1, ...transform(i, newLines[i].length - 1)])
+          }else{
+            newLines[i].push(Object.assign({}, lines[x1][y1]))
+            if(newLines[i].length - 1 !== j ){
+              animation.push([x1, y1, ...transform(i, newLines[i].length - 1 )])
+            }
+          }
+        }
       }
     }
+    console.log(newLines)
   }
 
   useEffect(() => {
     const listener = (e) => {
-      console.log(e.keyCode)
+      const { keyCode } = e;
+      if(keyCode === 40){
+        handleMove()
+      }
     }
 
     document.addEventListener('keydown', listener)
--- src/pages/games/tetris/index.tsx
+++ src/pages/games/tetris/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './style.less';
 
-export default () => {
+const Tetris = () => {
   return (
     <div>
       俄罗斯方块哦1
     </div>
   )
 }
+
+export default Tetris;
